858dcdf0ca242852a5e96cde41b3ab0eb31eceac
Erik Johansson <pg-wannabe@hotmail.com>
2012-04-19 16:17:57 +0200

    Uses file_get_contents() if curl isn't available

--- Api/Spotify-Request-handler.php
+++ Api/Spotify-Request-handler.php
@@ -9,21 +9,26 @@
         
         if ( isset($searchtype) && isset($searchquery) )
         {
-        	$response_list = array();
         	// get the response
-			$url = "http://ws.spotify.com/search/1/".$searchtype.".json?q=".$searchquery;			
-			$curl;
-			$curl = curl_init();
-	 
-			curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 2);
-			curl_setopt($curl, CURLOPT_HEADER, false);
-			curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-			curl_setopt($curl, CURLOPT_URL, $url);
-	 
-			$response = json_decode(curl_exec($curl));
-			curl_close($curl);	
+			$url = "http://ws.spotify.com/search/1/".$searchtype.".json?q=".$searchquery;	            
+            if( function_exists('curl_version') == "Enabled" )
+            {
+    			$curl;
+    			$curl = curl_init();
+    	 
+    			curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 2);
+    			curl_setopt($curl, CURLOPT_HEADER, false);
+    			curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    			curl_setopt($curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+    			curl_setopt($curl, CURLOPT_URL, $url);
+    	 
+    			$response = json_decode(curl_exec($curl));
+    			curl_close($curl);	
+            }else{
+                $res = file_get_contents($url);
+                $response = json_decode($res);
+            }
             // write it as json
             header('Cache-Control: no-cache, must-revalidate');
             header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
@@ -40,7 +45,7 @@
     {
         $response = array('error' => true, 'message' => 'please login');
         header('HTTP/1.1 500 Internal Server Error');
-    }
+    }   
     
-    echo json_encode($response);
+    echo json_encode($response);   
 ?>
